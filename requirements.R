51fe7d3bef22fc757e7fda35cf491c233a308943
Josep <>
2021-11-05 10:45:58 +0100

    install libraries locally

--- requirements.R
+++ requirements.R
@@ -1,4 +1,7 @@
-install.packages("tidyverse", lib = "../R/library")
-install.packages("stars", lib = "../R/library")
-install.packages("parallel", lib = "../R/library")
-remotes::install_github("icra/edibleCity", lib = "../R/library")
+repo_url <-  "http://cran.us.r-project.org"
+local_folder <- "../R/library"
+
+install.packages("tidyverse", lib = local_folder , repos = repo_url)
+install.packages("stars", lib = local_folder , repos = repo_url)
+install.packages("parallel", lib = local_folder , repos = repo_url)
+remotes::install_github("icra/edibleCity", lib = local_folder)
