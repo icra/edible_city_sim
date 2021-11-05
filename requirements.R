1a4681e6d2687817867834e24abfbbc18cd29712
Josep <>
2021-11-05 11:17:32 +0100

    add conditions to requirements

--- requirements.R
+++ requirements.R
@@ -1,7 +1,13 @@
 repo_url <-  "http://cran.us.r-project.org"
 local_folder <- "../R/library"
 
-install.packages("tidyverse", lib = local_folder , repos = repo_url)
-install.packages("stars", lib = local_folder , repos = repo_url)
-install.packages("parallel", lib = local_folder , repos = repo_url)
-remotes::install_github("icra/edibleCity", lib = local_folder)
+if(!require(tidyverse, lib.loc = local_folder)) 
+  install.packages("tidyverse", lib = local_folder , repos = repo_url)
+if(!require(stars, lib.loc = local_folder)) 
+  install.packages("stars", lib = local_folder , repos = repo_url)
+if(!require(parallel, lib.loc = local_folder)) 
+  install.packages("parallel", lib = local_folder , repos = repo_url)
+if(!require(ediblecity, lib.loc = local_folder)) 
+  remotes::install_github("icra/edibleCity", lib = local_folder)
+
+print("All installed")
